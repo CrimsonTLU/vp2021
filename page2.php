c862bb2fcf9e485dc1defa2dcb17c77c8f834269
Christian Hindremäe <CrimsonTLU@users.noreply.github.com>
2021-09-21 17:14:45 +0300

    Kodune töö

--- page2.php
+++ page2.php
@@ -1,61 +1,86 @@
 <?php
 	$author_name = "Christian Hindremäe";
 	
-	//kontrollin, kas POST info jõuab kuhugi
+	//vaatan, mida POST meetodil saadeti
 	//var_dump($_POST);
-	//kontrollin, kas klikiti Submit nuppu
-	$todays_adjective_html = null;
-	$todays_adjective_error = null;
+	
+	$today_html = null; //$today_html = "";
+	$today_adjective_error = null;
 	$todays_adjective = null;
-	if(isset($_POST["adjective_submit"])) {
-		//echo "Klikiti!";
-		//<p>Tänane päev on tuuline.</p>
-		//kontrollime, kas midagi kirjutati ka
-		if(!empty($_POST["todays_adjective_input"])) {
-			$todays_adjective_html = "<p>Tänane päev on " .$_POST["todays_adjective_input"] .".</p>";
+	//kontrollin, kas klikiti submit
+	if(isset($_POST["submit_todays_adjective"])){
+		//echo "Klikiti nuppu!";
+		if(!empty($_POST["todays_adjective_input"])){
+			$today_html = "<p>Tänane päev on " .$_POST["todays_adjective_input"] .".</p>";
 			$todays_adjective = $_POST["todays_adjective_input"];
 		} else {
-			$todays_adjective_error = "Palun sisesta tänase kohta sobiv omadussõna!";
+			$today_adjective_error = "Palun kirjutage tänase kohta omadussõna!";
 		}
 	}
-	//juhusliku foto lisamine
+	
+	//lisan lehele juhusliku foto
 	$photo_dir = "photos/";
-	//loen kataloogi sisu
-	$all_files = scandir($photo_dir);
-	$all_real_files = array_slice($all_files, 2);
+	$all_files = array_slice(scandir($photo_dir), 2);
 	
-	//sõelume välja päris pildid
-	$photo_files = [];
+	//kontrollin ja võtan ainult fotod
 	$allowed_photo_types = ["image/jpeg", "image/png"];
-	foreach($all_real_files as $file_name) {
-		$file_info = getimagesize($photo_dir .$file_name);
-		if(isset($file_info["mime"])) {
-			if(in_array($file_info["mime"], $allowed_photo_types)) {
-				array_push($photo_files, $file_name);
-			}//if(in_array)
-		}//if(isset) lõppeb
-	}//foreach lõppes
+	$all_photos = [];
+	foreach($all_files as $file){
+		$file_info = getimagesize($photo_dir .$file);
+		if(isset($file_info["mime"])){
+			if(in_array($file_info["mime"], $allowed_photo_types)){
+				array_push($all_photos, $file);
+			}//if in_array lõppeb
+		}//if isset lõppeb
+	}//foreach lõppeb
 	
-	//var_dump($all_real_files);
-	//loen massiivi elemendid kokku
-	$file_count = count($photo_files);
-	//loosin juhusliku arvu (min peab olema 0 ja max count - 1)
+	$file_count = count($all_photos);
 	$photo_num = mt_rand(0, $file_count - 1);
-	//<img src="kataloog/fail" alt="Tallinna Ülikool">
-	$photo_html = '<img src="' .$photo_dir .$photo_files[$photo_num] .'" alt="Tallinna Ülikool">';
+    
+    if(isset($_POST["photo_select_submit"])){
+		$photo_num = $_POST["photo_select"];
+	}
+    
+	$photo_html = '<img src="' .$photo_dir .$all_photos[$photo_num] .'" alt="Tallinna Ülikool">';
+	$photo_file_html = "\n <p>".$all_photos[$photo_num] ."</p> \n";
+    
+    $photo_list_html = "\n <ul> \n";
 	
-	$photo_list_html = "\n <ul> \n";
-	for($i = 0;$i < $file_count;$i ++) {
-		$photo_list_html .= "<li>" .$photo_files[$i] ."</li> \n";
+	//tsükkel
+	//for($i=algväärtus; $i < piirväärtus; $i muutumine){...}
+	
+	//<ul>
+	//<li>pildifail1.jpg</li>
+	//...
+	//<li>pildifailn.jpg</li>
+	//</ul>
+	
+	for($i = 0; $i < $file_count; $i ++){
+		$photo_list_html .= "<li>" .$all_photos[$i] ."</li> \n";
 	}
 	$photo_list_html .= "</ul> \n";
 	
-	$photo_select_html = "\n" .'<select name="photo_select">' ."\n";
-	for($i = 0;$i < $file_count;$i ++){
-		$photo_select_html .= '<option value="' .$i .'">' .$photo_files[$i] ."</option> \n";
+/* 	<select name="photo_select">
+		<option value="0">tlu_astra_600x400_1.jpg</option> 
+		<option value="1">tlu_astra_600x400_2.jpg</option> 
+		<option value="2">tlu_hoov_600x400_1.jpg</option> 
+		<option value="3">tlu_mare_600x400_1.jpg</option> 
+		<option value="4">tlu_mare_600x400_2.jpg</option> 
+		<option value="5">tlu_terra_600x400_1.jpg</option> 
+		<option value="6">tlu_terra_600x400_2.jpg</option> 
+		<option value="7">tlu_terra_600x400_3.jpg</option> 
+	</select>  */
+	
+	$photo_select_html = '<select name="photo_select">' ."\n";
+	for($i = 0; $i < $file_count; $i ++){
+		$photo_select_html .= '<option value="' .$i .'"';
+        if($i == $photo_num){
+			$photo_select_html .= " selected";
+		}
+        $photo_select_html .= '>' .$all_photos[$i] ."</option> \n";
 	}
 	$photo_select_html .= "</select> \n";
-
+	
 ?>
 <!DOCTYPE html>
 <html lang="et">
@@ -64,25 +89,29 @@
 	<title><?php echo $author_name; ?>, veebiprogrammeerimine</title>
 </head>
 <body>
-	<h1>Esimene lehekülg, muudatused tehtud kodus</h1>
-	<p>See lehekülg on loodud õppetöö raames ega ei sisalda tõsiseltvõetavat sisu!</p>
-	<p>Õppetöö toimub <a href="https://www.tlu.ee/dt">Tallinna Ülikooli Digitehnoloogiate Instituudis</a>.</p>
+	<h1><?php echo $author_name; ?>, veebiprogrammeerimine, muudatused tehtud kodus</h1>
+	<p>See leht on valminud õppetöö raames ja ei sisalda mingit tõsiseltvõetavat sisu!</p>
+	<p>Õppetöö toimub <a href="https://www.tlu.ee/dt">Tallinna Ülikooli Digitehnoloogiate instituudis</a>.</p>
 	<hr>
+	<!--ekraanivorm-->
 	<form method="POST">
-		<input type="text" placeholder="omadussõna päeva kohta" name="todays_adjective_input" value="<?php echo $todays_adjective; ?>">
-		<input type="submit" name="adjective_submit" value="Saada">
-		<span><?php echo $todays_adjective_error; ?></<span>
-	</form>	
-	<?php echo $todays_adjective_html; ?>
+		<input type="text" name="todays_adjective_input" placeholder="tänase päeva ilma omadus" value="<?php echo $todays_adjective; ?>">
+		<input type="submit" name="submit_todays_adjective" value="Saada">
+		<span><?php echo $today_adjective_error; ?></span>
+	</form>
+	<?php echo $today_html; ?>
 	<hr>
+	
 	<form method="POST">
 		<?php echo $photo_select_html; ?>
+        <input type="submit" name="photo_select_submit" value="Vali">
 	</form>
-	<hr>
-	<?php 
-		echo $photo_html;
-		echo $photo_list_html;
-		?>
 	
+	<?php
+		echo $photo_html;
+        echo $photo_file_html;
+		echo "<hr> \n";
+		echo $photo_list_html;
+	?>
 </body>
 </html>
